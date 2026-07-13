4bf84363c72976f8904c1b658b1c35157041dc3f
Jasaman Singh Chaggar <jsc.21905@gmail.com>
2026-07-13 23:33:42 +0530

    Increase EBS volume size from 20GB to 30GB in launch template

--- infra/terraform/ec2.tf
+++ infra/terraform/ec2.tf
@@ -45,7 +45,7 @@ resource "aws_launch_template" "app" {
   device_name = "/dev/xvda"
 
     ebs {
-      volume_size           = 20
+      volume_size           = 30
       volume_type           = "gp3"
       delete_on_termination = true
     }
